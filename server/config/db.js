9725b663d09df5e05946186ac72b266b69d098f5
elifnalan <emutlu1@cougars.ccis.edu>
2026-04-25 16:12:57 -0500

    fix: use mysql pool and add db config

--- server/config/db.js
+++ server/config/db.js
@@ -1,19 +1,21 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
+// Create a MySQL connection pool
+// pool is for ensuring several connections can be handled at once, 
+// and it will manage the connections for us
+
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 3306
+    port: 3306,
+    connectionLimit: 10,
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('MySQL connection error:', err);
-        return;
-    }
-    console.log('Connected to MySQL database!');
+db.query('SELECT 1', (err) => {
+    if (err) console.error('MySQL connection error:', err);
+    else console.log('Connected to MySQL database!');
 });
 
 module.exports = db;
--- server/server.js
+++ server/server.js
@@ -9,29 +9,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(express.json());
+
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 
 const port = process.env.PORT || 50000;
-
-// MySQL connection configuration
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: 3306
-});
-
-// Ensuring the MySQL connection is established
-db.connect((err) => {
-    if (err) {
-        console.error("MySQL connection error:", err);
-        return;
-    }
-    console.log("Connected to MySQL database!");
-});
-
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
