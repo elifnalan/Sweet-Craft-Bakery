002150caa63bb61962829e7a84bfc1d78851d1ea
SayuriShrestha <160147685+SayuriShrestha@users.noreply.github.com>
2026-04-30 20:42:27 -0500

    add orders route and fix server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 const path = require('path');
+const db = require('./config/db');
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,15 +14,17 @@ app.use(express.json());
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 
-const port = process.env.PORT || 50000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const orderRoutes = require('./routes/orders');
+app.use('/api/orders', orderRoutes);
 
-// for fetching the dessertss from the database and sending it to the client
 app.get('/api/desserts', (req, res) => {
     db.query('SELECT * FROM desserts', (err, results) => {
         if (err) return res.status(500).json({ error: err });
         res.json(results);
     });
+});
+
+const port = process.env.PORT || 50000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
