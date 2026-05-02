012808c75885cc008d860a8db13e972c8d10cb93
SayuriShrestha <160147685+SayuriShrestha@users.noreply.github.com>
2026-05-01 21:31:43 -0500

    order backend and add cart badge to navbar and fix sticky nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
-import logo from '../assets/logo.png'; // ← add this
+import logo from '../assets/logo.png';
+import { useCart } from '../context/CartContext';
 
-function Navbar() { 
-    return(
+function Navbar() {
+    const { cart } = useCart();
+    const cartCount = cart.reduce((total, item) => total + item.quantity, 0);
+
+    return (
         <nav className='navbar'>
-
-           <div className='logo'>
-            <img src={logo} alt='logo' className='logo-icon' />
-            <h1 className='logo-text'>Sweet Craft Bakery</h1>
+            <div className='logo'>
+                <img src={logo} alt='logo' className='logo-icon' />
+                <h1 className='logo-text'>Sweet Craft Bakery</h1>
             </div>
-            
-            <ul className='nav-links'>  
+
+            <ul className='nav-links'>
                 <li><Link to='/'> Home </Link></li>
                 <li><Link to='/bakeacake'> Bake a Cake </Link></li>
                 <li><Link to='/desserts'> Desserts </Link></li>
-                <li><Link to='/cart'> Cart </Link></li>
+                <li>
+                    <Link to='/cart' className='cart-link'>
+                        🛒
+                        {cartCount > 0 && (
+                            <span className='cart-badge'>{cartCount}</span>
+                        )}
+                    </Link>
+                </li>
                 <li><Link to='/login'> Login </Link></li>
             </ul>
         </nav>
